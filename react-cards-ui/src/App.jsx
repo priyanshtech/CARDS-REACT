9150cbdcff7ea04a4a9f8cc645e64359b1666644
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-11-30 15:04:53 +0530

    made the basic skeleton of the website

---
+++ react-cards-ui/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "./components/card";
+
+export default function App() {
+  return (
+    <main>
+      <h1>React Cards UI</h1>
+      <Card />
+    </main>
+  );
+}
---
+++ react-cards-ui/src/data/Cards.jsx
@@ -0,0 +1,26 @@
+export const cards = [
+{
+id: 1,
+title: "Aurora Headphones",
+subtitle: "Wireless • Active Noise Canceling",
+price: "₹5,499",
+rating: 4.6,
+img: "https://images.unsplash.com/photo-1518444027278-9cb0b9dc6f0e?auto=format&fit=crop&w=800&q=60"
+},
+{
+id: 2,
+title: "Nimble Backpack",
+subtitle: "15L • Water-resistant",
+price: "₹1,799",
+rating: 4.2,
+img: "https://www.titan.co.in/dw/image/v2/BKDD_PRD/on/demandware.static/-/Sites-titan-master-catalog/default/dwb830e1d4/images/Titan/Catalog/1805QM04_1.jpg?sw=600&sh=600"
+},
+{
+id: 3,
+title: "Solar Smartwatch",
+subtitle: "Fitness • 10-day battery",
+price: "₹9,999",
+rating: 4.8,
+img: "https://images.unsplash.com/photo-1516574187841-cb9cc2ca948b?auto=format&fit=crop&w=800&q=60"
+}
+];
