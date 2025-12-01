bd84b2a03b2d476228d11f158c597977e368576f
Priyansh Khatri <priyanshkhatri8690@gmail.com>
2025-12-01 14:20:39 +0530

    added css to both the files , app.jsx and card.jsx , practiced tailwind , props concepts are getting stronger , hooks(useState) is also getting stronger

--- react-cards-ui/src/App.jsx
+++ react-cards-ui/src/App.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import { cards } from "./data/cards";
 import Card from "./components/card";
 
 export default function App() {
+    const[cartCount, setCartCount] = useState(0);
+    function handleAddToCart() {
+        setCartCount(cartCount + 1);
+    }
+
   return (
-    <main>
-      <h1>React Cards UI</h1>
+    <main className="min-h-screen flex flex-col bg-gray-100 p-6">
+      <header className="max-w-5xl mx-auto mb-10">
+        <h1 className="text-5xl font-bold">React Cards UI</h1>
+        <p className="text-gray-600 text-2xl mt-2">Cart: {cartCount}</p>
+      </header>
 
-      <section >
+      <section className="max-w-5xl mx-auto grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        
+     
         {cards.map((item) => (
-            <Card{...item}
-            />))}
-
-             
+          <Card key={item.id} {...item} onAdd={handleAddToCart}/>
+        ))}
       </section>
     </main>
   );
--- react-cards-ui/src/components/Card.jsx
+++ react-cards-ui/src/components/Card.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
 export default function Card(props) {
-  const { title, subtitle, price, rating, img } = props;
+  const { title, subtitle, price, rating, img ,onAdd } = props;
 
   return (
-    <article>
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{subtitle}</p>
-      <div>{price}</div>
-      <div>{rating}</div>
+    <article className="bg-white rounded-xl shadow-lg overflow-hidden">
+      <figure> 
+        <img className="object-cover w-full h-50"src ={img} alt={title} />
+      </figure>
+      <header className="p-4">
+        <h2 className="text-lg font-semibold">{title}</h2>
+        <p className="text-gray-500 text-sm mt-1">{subtitle}</p>
+      </header>
+        <section className="px-5"> 
+            <p className="text-md font-medium">Price: {price}</p>
+            <p className="text-sm text-gray mt-1">Rating: {rating}</p>
+        </section>
+        <footer className="p-4 ">
+            <button onClick={onAdd} className=" w-full py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-800 transition">Add to cart</button>
+            
+        </footer>
     </article>
   );
 }
